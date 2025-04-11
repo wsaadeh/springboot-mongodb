bcd20c53b789970ee867cfd3e5449292fcf385aa
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-04-11 13:10:05 -0300

    Entity User and Rest working

---
+++ src/main/java/com/saadeh/springbootmongodb/rosources/UserResource.java
@@ -0,0 +1,24 @@
+package com.saadeh.springbootmongodb.rosources;
+
+import com.saadeh.springbootmongodb.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> finAll(){
+       User maria = new User("1","Maria Brown","maria@gmail.com");
+       User alex = new User("2","Alex Green","alex@gmail.com");
+       List<User> u = new ArrayList<>();
+       u.addAll(Arrays.asList(maria, alex));
+       return ResponseEntity.ok().body(u);
+    }
+}
