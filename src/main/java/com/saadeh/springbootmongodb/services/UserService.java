158b3e364558a8c7e13a87c7e075ed20713c4544
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-04-14 10:44:23 -0300

    Delete user from service using DELETE operation

--- src/main/java/com/saadeh/springbootmongodb/services/UserService.java
+++ src/main/java/com/saadeh/springbootmongodb/services/UserService.java
@@ -29,6 +29,11 @@ public class UserService {
         return repo.insert(obj);
     }
 
+    public void delete(String id){
+        findById(id);
+        repo.deleteById(id);
+    }
+
     public User fromDto(UserDto objDto){
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
