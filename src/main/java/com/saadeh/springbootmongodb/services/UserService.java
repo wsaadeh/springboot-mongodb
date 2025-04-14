68c86756e4ecee22e285ab838b68724dd2aeee73
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-04-14 11:00:59 -0300

    Update user using PUT

--- src/main/java/com/saadeh/springbootmongodb/services/UserService.java
+++ src/main/java/com/saadeh/springbootmongodb/services/UserService.java
@@ -34,6 +34,17 @@ public class UserService {
         repo.deleteById(id);
     }
 
+    public User update(User obj){
+        User newObj = repo.findById(obj.getId()).get();
+        updateData( newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     public User fromDto(UserDto objDto){
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
