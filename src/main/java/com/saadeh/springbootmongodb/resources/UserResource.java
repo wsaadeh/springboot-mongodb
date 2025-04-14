e07614cb034fb3db827e1754a06d3f7825759d79
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-04-14 10:32:36 -0300

    Insert user using POST

--- src/main/java/com/saadeh/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/saadeh/springbootmongodb/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.saadeh.springbootmongodb.dto.UserDto;
 import com.saadeh.springbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -33,5 +32,13 @@ public class UserResource {
     public ResponseEntity<UserDto> finById(@PathVariable String id){
         User u = service.findById(id);
         return ResponseEntity.ok().body(new UserDto(u));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDto objDto){
+        User obj = service.fromDto(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/saadeh/springbootmongodb/services/UserService.java
+++ src/main/java/com/saadeh/springbootmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.saadeh.springbootmongodb.services;
 
 import com.saadeh.springbootmongodb.domain.User;
+import com.saadeh.springbootmongodb.dto.UserDto;
 import com.saadeh.springbootmongodb.repository.UserRepository;
 import com.saadeh.springbootmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(()-> new ObjectNotFoundException("Object not found!"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDto(UserDto objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
